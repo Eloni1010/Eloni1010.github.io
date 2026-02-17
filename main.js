734c97dc887340ebefbda02fe9963c0c35100690
Lynn Port <Lport@ewu.edu>
2026-02-16 17:27:10 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  document.documentElement.dataset.theme = savedTheme;
+}
+  const toggle = document.querySelector('.theme-toggle');
+toggle.addEventListener('click', function() {
+  const currentTheme = document.documentElement.dataset.theme;
+  let newTheme;
+  if (currentTheme === 'dark') {
+    newTheme = 'light';
+  } else {
+    newTheme = 'dark';
+  }
+  document.documentElement.dataset.theme = newTheme;
+  localStorage.setItem('theme', newTheme);
+});
